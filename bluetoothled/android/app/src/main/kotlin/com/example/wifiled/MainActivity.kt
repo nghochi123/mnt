069c9223a2cf312f1b2a41ff89fda37b288f8fc9
nghochi123 <nghochi123@gmail.com>
2022-08-05 21:48:57 +0800

    cleanup

--- bluetoothled/android/app/src/main/kotlin/com/example/wifiled/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wifiled
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mnt_app/android/app/src/main/kotlin/com/example/mnt_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mnt_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
